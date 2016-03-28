2b15e55b6821fb6c4fd507c6116fc65bc1348f32
Paulo Gouveia <paulojlgouveia@gmail.com>
2016-03-28 12:26:43 +0100

    ping working on tranporter

--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,7 +1,54 @@
 package pt.upa.transporter.ws;
 
-public class TransporterPort {
+import java.util.ArrayList;
+import java.util.List;
 
-	// TODO
+import javax.jws.WebService;
 
+// http://localhost:8081/transporter-ws/endpoint?wsdl
+
+@WebService(
+	endpointInterface="pt.upa.transporter.ws.TransporterPortType",
+    wsdlLocation="transporter.1_0.wsdl",
+    name="UpaTransporter1",
+    portName="TransporterPort",
+    targetNamespace="http://ws.transporter.upa.pt/",
+    serviceName="TransporterService"
+)
+public class TransporterPort implements TransporterPortType {
+
+	@Override
+	public String ping(String name) {
+		return "Pong " + name + "!";
+	}
+	
+	@Override
+    public JobView requestJob(String origin, String destination, int price)
+            throws BadLocationFault_Exception, BadPriceFault_Exception {
+		
+		return new JobView();
+	}
+	
+	@Override
+    public JobView decideJob(String id,boolean accept)
+            throws BadJobFault_Exception {
+		
+    	return new JobView();
+
+    }
+	
+	@Override
+	public JobView jobStatus(String id) {
+		return new JobView();
+	}
+	
+	@Override
+	public List<JobView> listJobs() {		 
+		return  new ArrayList<JobView>();
+	}
+	
+	@Override
+	public void clearJobs() {
+		
+	}
 }
