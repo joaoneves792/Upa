b98e31192b86a2fd6affd95a4332676a85270945
Joao Neves <joaoneves792@gmail.com>
2016-05-04 15:07:19 +0100

    Created a KeyManager, we can now use the privatekeys and certs in the handler

---
+++ ws-handlers/src/main/java/pt/upa/ws/handler/KeyManager.java
@@ -0,0 +1,88 @@
+package pt.upa.ws.handler;
+
+import pt.upa.a45.CA.cli.CAClient;
+import pt.upa.a45.CA.cli.CAException;
+
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * Created by joao on 5/4/16.
+ */
+public class KeyManager {
+
+
+    private static final char[] PASSWORD = "123456".toCharArray();
+
+    private static final String UDDI_URL = "http://localhost:9090"; /*This shouldnt be a constant, but I dont see any other way (First incoming message)*/
+
+    private static KeyStore _ks;
+    private static CAClient _ca;
+
+    private static KeyManager instance = null;
+
+    private KeyManager(String keystore){
+        _ks = loadKeystore(keystore, PASSWORD);
+        initializeCAClient();
+    }
+
+    private KeyManager(){
+        initializeCAClient();
+    }
+
+    public static KeyManager getInstance(String keystore){
+        if(null == instance)
+            if(null != keystore)
+                instance = new KeyManager(keystore);
+            else
+                instance = new KeyManager();
+        else if(null == _ks && null != keystore)
+            _ks = loadKeystore(keystore, PASSWORD);
+        return instance;
+    }
+
+    private static KeyStore loadKeystore(String name, char[] password){
+        System.out.println("[Handler]: Loading keystore for: "+ name);
+
+        FileInputStream fis;
+        String filename = name + ".jks";
+        try {
+            fis = new FileInputStream(filename);
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keystore.load(fis, password);
+            return keystore;
+        } catch (FileNotFoundException e) {
+            System.err.println("Keystore file <" + filename + "> not fount.");
+            System.exit(-1);
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException e){
+            System.err.println("Failed to load the Keystore" + e.getMessage());
+            System.exit(-1);
+        }
+        return null;
+    }
+    private static void initializeCAClient(){
+        try {
+            _ca = new CAClient(UDDI_URL);
+        } catch (CAException e) {
+            System.err.println("Failed to initializeKeyStore the CA client " + e.getMessage());
+            System.exit(-1);
+        }
+
+    }
+
+    public static PrivateKey getMyPrivateKey() throws NoSuchAlgorithmException, UnrecoverableEntryException, KeyStoreException{
+        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(PASSWORD);
+        return ((KeyStore.PrivateKeyEntry)(_ks.getEntry("mykey", protParam))).getPrivateKey();
+    }
+
+    public static X509Certificate getCertificate(String entity)throws CAException{
+        //TODO: implement a cache
+        return _ca.getCertificate(entity);
+    }
+
+}
--- ws-handlers/src/main/java/pt/upa/ws/handler/SignatureHandler.java
+++ ws-handlers/src/main/java/pt/upa/ws/handler/SignatureHandler.java
@@ -1,15 +1,7 @@
 package pt.upa.ws.handler;
 
-import pt.upa.a45.CA.cli.CAClient;
-import pt.upa.a45.CA.cli.CAException;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -40,55 +32,11 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext> {
 	public static final String CLASS_NAME = SignatureHandler.class.getSimpleName();
 	public static final String TOKEN = "client-handler";
 
-	private final char[] PASSWORD = "123456".toCharArray();
 
-	private final String UDDI_URL = "http://localhost:9090"; /*This shouldnt be a constant, but I dont see any other way (First incoming message)*/
-
-	private static KeyStore _ks = null;
-	private CAClient _ca;
+	private KeyManager _keyManager;
 
 	public Set<QName> getHeaders() {
 		return null;
-	}
-
-	private KeyStore loadKeystore(String filename, char[] password){
-		FileInputStream fis;
-		try {
-			fis = new FileInputStream(filename);
-			KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-			keystore.load(fis, password);
-			return keystore;
-		} catch (FileNotFoundException e) {
-			System.err.println("Keystore file <" + filename + "> not fount.");
-			System.exit(-1);
-		} catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException e){
-			System.err.println("Failed to load the Keystore" + e.getMessage());
-			System.exit(-1);
-		}
-		return null;
-	}
-
-	private void initializeKeyStore(SOAPMessageContext smc){
-		String name = (String)smc.get("wsName");
-		//String uddiURL = (String)smc.get("uddiURL");
-		
-		System.out.println("[Handler] We are being requested by: " + name);
-		
-		if(name.equals("transporterClient"))
-			return;
-		
-		loadKeystore(name + ".jks", PASSWORD);
-
-	}
-
-	private void initializeCAClient(){
-		try {
-			_ca = new CAClient(UDDI_URL);
-		} catch (CAException e) {
-			System.err.println("Failed to initializeKeyStore the CA client " + e.getMessage());
-			System.exit(-1);
-		}
-
 	}
 
 
@@ -97,15 +45,28 @@ public class SignatureHandler implements SOAPHandler<SOAPMessageContext> {
 		Boolean outbound = (Boolean) smc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
 		
 		/*Initialize the keystore and CA client*/
-		if(_ks == null && outbound) {/* If its inbound then we dont need the keystore*/
-			initializeKeyStore(smc);
-		}
-		if(null == _ca){
-			initializeCAClient();
+		if(outbound) {/* If its inbound then we dont need the keystore (and in fact we cant even know which one is outs)*/
+			String name = (String)smc.get("wsName");
+			_keyManager = KeyManager.getInstance(name);
+		}else{
+			_keyManager = KeyManager.getInstance(null);
 		}
 		
 		// outbound //
 		if (outbound) {
+
+			/*THIS is an example on how to get your own private key and everyone else's certificates -->*/
+			try {
+				PrivateKey mykey = _keyManager.getMyPrivateKey(); //You can only get your private key when message is outbound
+				X509Certificate cert1 = _keyManager.getCertificate("UpaTransporter1"); //Certs you can get whenever you want
+				X509Certificate cert2 = _keyManager.getCertificate("UpaTransporter2");
+				X509Certificate cert3 = _keyManager.getCertificate("UpaBroker");
+			}catch (Exception e){
+				System.out.println(e.toString());
+				System.exit(-1);
+			}
+			/*<----*/
+
 			// get token from request context
 			String propertyValue = (String) smc.get(REQUEST_PROPERTY);
 			System.out.printf("%s received '%s'%n", CLASS_NAME, propertyValue);
